f2c04009015c396880064583448d3ea1e2b2504b
Francisco Arbos Ortega <curroarbos@gmail.com>
2023-11-15 10:14:14 +0800

    done lists controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,14 @@
 require "json"
 require "open-uri"
 
+Movie.destroy_all
+
 url = "https://tmdb.lewagon.com/movie/top_rated"
 movies_serialized = URI.open(url).read
 movies = JSON.parse(movies_serialized)['results']
 movies.each do |movie|
   Movie.create(
-    title: movie['original_title'],
+    title: movie['title'],
     overview: movie['overview'],
     poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
     rating: movie['vote_average']
